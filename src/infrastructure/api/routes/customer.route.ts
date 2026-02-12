c168f80ebea338df9d26d5b5a26805c5e928f1c5
ThomasDixini <thomasdixini.dev@gmail.com>
2026-02-12 20:40:07 -0300

    feat: :sparkles: Creating first application's endpoint

---
+++ src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,27 @@
+import express, { Router } from "express";
+import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
+import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
+import { number } from "yup";
+
+
+export const customerRoute: Router = express.Router();
+
+customerRoute.post("/", async (req, res) => {
+    const useCase = new CreateCustomerUseCase(new CustomerRepository());
+    try
+    {
+        const customerDto = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                number: req.body.address.number,
+                city: req.body.address.city,
+                zip: req.body.address.zipCode
+            }
+        };
+        const output = await useCase.execute(customerDto);
+        res.status(200).send(output);
+    } catch(err){
+        res.status(500).send(err);
+    }
+});
---
+++ src/infrastructure/api/tests/customer.e2e.spec.ts
@@ -0,0 +1,45 @@
+import request from 'supertest';
+import { app, sequelize } from '../express';
+import { number } from 'yup';
+
+describe("E2E Test for Customer", () => {
+
+    beforeEach(async () => {
+        await sequelize.sync({ force: true });
+    });
+
+    afterAll(async () => {
+        await sequelize.close();
+    });
+
+    it("should create a customer", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name: "John Doe",
+                address: {
+                    street: "123 Main St",
+                    city: "Anytown",
+                    number: 100,
+                    zipCode: "12345"
+                }
+            });
+
+            expect(response.status).toBe(200);
+            expect(response.body.name).toBe("John Doe");
+            expect(response.body.address.street).toBe("123 Main St");
+            expect(response.body.address.city).toBe("Anytown");
+            expect(response.body.address.number).toBe(100);
+            expect(response.body.address.zip).toBe("12345");
+    })
+
+    it("should not create a customer", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name: "John Doe",
+            });
+
+            expect(response.status).toBe(500);
+    });
+})
